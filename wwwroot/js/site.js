36678d1508f7882a5038e920be8f7a22dd0091bf
Phakama Ntshewula <Phakama@construction-computer-software.com>
2018-01-16 15:30:29 +0200

    Added user menu for adding and retrieving Users

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,27 +5,8 @@ $(document).ready(function () {
     });
 
     $("#userlist").DataTable({
-        ajax: "../AppUser/UsersList"
-        //processing: true,
-        //serverSide: true,
-        //rowReorder: true,
-        //deferRender: true,
-        //lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
-        //sAjaxSource: "../AppUser/UsersList",
-        //columns: [
-        //    { data: "FirstName" },
-        //    { data: "LastName" },
-        //    { data: "CellNumber" },
-        //    { data: "EmailAddress" }
-        //]
-        //, fnDrawCallback: function () {
-
-        //    $('#errorTable tbody tr').click(function () {
-        //        var data = table.row($(this)).data();
-        //        var id = JSON.stringify(data["candyErrId"]);
-        //        document.location.href = '../Error/index/' + id;
-        //    });
-        //} 
+        "columnDefs": [
+            { "orderable": false, "targets": [2, 3, 4] }
+        ]
     });
-
 });
